00a119a1c2f774bfee93812a64e72c893a6f943c
Dquay <anhquocpm99@gmail.com>
2025-03-18 23:36:52 +0700

    update3

--- src/Buoi5/BaiTap/Module1/Phan4/Bai1.java
+++ src/Buoi5/BaiTap/Module1/Phan4/Bai1.java
@@ -6,6 +6,13 @@ public class Bai1 {
     public static void main(String[] args) {
         System.out.println("Nhap so luong phan tu trong mang muon nhap :");
         int number = new Scanner(System.in).nextInt();
-        
+        int n = new Scanner(System.in).nextInt();
+        int[] array = new int[n];
+        System.out.println("Nhập các phần tử của mảng: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = new Scanner(System.in).nextInt();
+        }
+
+
     }
 }
